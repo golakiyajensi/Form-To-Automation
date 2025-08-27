6e5236c01df9d832372a769bc1e3f5a79465fd8e
Margi Oza <margioza7@gmail.com>
2025-08-27 10:47:49 +0530

    Created sign in page, password page and reset password page.
    Created sign in page, password page and reset password page.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // required for dropdown behavior
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "./CSS/style.css";
+
+import Signin from "./Components/Signin.jsx";
+import UseAccount from "./Components/UseAccount.jsx";
+import Password from "./Components/Pasword.jsx";
+import ForgotPassword from "./Components/ForgotPassword.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path='/signin' element={<Signin/>}></Route>
+          <Route path='/useaccount' element={<UseAccount/>}></Route>
+          <Route path='/password' element={<Password/>}></Route>
+          <Route path='/forgotpassword' element={<ForgotPassword/>}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
 
-export default App
+export default App;
